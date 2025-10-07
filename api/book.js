643fde8ab4464daa749ac6bffbe94256e9331787
c-neal5 <coltonneal25@gmail.com>
2025-10-07 13:22:37 -0500

    Create book.js

---
+++ api/book.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+  const { customer_name, phone, address, service_list, start, staff } = req.body || {};
+
+  return res.json({
+    ok: true,
+    booking_id: "TRC-" + Math.floor(Math.random() * 100000),
+    ics_url: "https://calendar.google.com/"
+  });
+}
