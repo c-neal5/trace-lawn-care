8c73e7ab14fec79ce78ad40d7749700292769349
c-neal5 <coltonneal25@gmail.com>
2025-10-07 13:23:15 -0500

    Create modify.js

---
+++ api/modify.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+  const { action, booking_id, new_start } = req.body || {};
+
+  return res.json({ ok: true, action, booking_id, new_start });
+}
