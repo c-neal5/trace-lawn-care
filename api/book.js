db3405edadff1f9898ea873fb4c00dcbf1027025
c-neal5 <coltonneal25@gmail.com>
2025-10-09 11:49:23 -0500

    book.js

--- api/book.js
+++ api/book.js
@@ -1,3 +1,4 @@
+import { auth, resolveCalendarId } from "./lib/calendar.js";
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
   const { customer_name, phone, address, service_list, start, staff } = req.body || {};
