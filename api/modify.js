5f1f13f51c2c0e4950059722c581b2ea23d62ad4
c-neal5 <coltonneal25@gmail.com>
2025-10-09 11:49:53 -0500

    modify.js

--- api/modify.js
+++ api/modify.js
@@ -1,3 +1,4 @@
+import { auth, resolveCalendarId } from "./lib/calendar.js";
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
   const { action, booking_id, new_start } = req.body || {};
